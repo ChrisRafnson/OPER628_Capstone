ef44f6873b63066b66c4faeb8305192efc9d0d6b
ChrisRafnson <90478268+ChrisRafnson@users.noreply.github.com>
2024-10-29 11:23:32 -0400

    Add files via upload

---
+++ sorts.py
@@ -0,0 +1,19 @@
+#@author 2d Lt Christopher Rafnson
+
+def merge():
+    pass
+
+def merge_sort(arr):
+    pass
+
+def selection_sort(arr):
+    pass
+
+def heap_sort(arr):
+    pass
+
+def quick_sort(arr):
+    pass
+
+def bucket_sort(arr):
+    pass
